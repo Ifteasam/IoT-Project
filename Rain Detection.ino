8dcf1233378e116d3f8a905488cc1434eb9c9ab7
Jahin Khan <jahinkhan1@gmail.com>
2024-04-03 01:58:08 +0600

    Add files via upload
    Rain detection IoT project utilizes various sensors and IoT technologies to detect rainfall in a specific area. It typically involves deploying rain sensors equipped with IoT capabilities to monitor precipitation levels in real-time. These sensors can detect rainfall through various mechanisms such as conductivity, optical sensing, or mechanical measurements.
    
    The data collected by these sensors is transmitted wirelessly to a central server or cloud platform, where it is processed and analyzed. By leveraging machine learning algorithms or simple threshold-based logic, the system can determine the onset, intensity, and duration of rainfall accurately.
    
    The applications of such a system are diverse, ranging from agricultural automation and irrigation management to urban infrastructure planning and flood monitoring. Rain detection IoT projects offer a cost-effective and efficient way to gather valuable environmental data, enabling better decision-making and resource management in various sectors.

---
+++ Rain Detection.ino
@@ -0,0 +1,16 @@
+//visit projects with mani youtube chanel for full tutorial video
+void setup() {
+  Serial.begin(9600);//enable serial monitor
+  pinMode(4, OUTPUT);//define LED pin
+}
+void loop() {
+  int value = analogRead(A3);//read value
+  Serial.print("Value : ");
+  Serial.println(value);
+  if (value < 400) {//check condition
+    digitalWrite(4, HIGH);
+    Serial.print("Heavy rain  LED on ");
+  } else {
+    digitalWrite(4, LOW);
+  }
+}
